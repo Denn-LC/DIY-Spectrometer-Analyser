265e3c9a5b4e1e9b757326db9414f27260dbefa5
Denn-LC <dennisonleadbetterclarke@gmail.com>
2026-03-10 18:39:43 +0000

    changed rbg to bgr as opencv uses

--- analyser.py
+++ analyser.py
@@ -20,24 +20,31 @@ def main():
             g_dist = []
             i_dist = []
             for i in range(shape[1]):
-                r_val = np.mean(cropped[:, i][:, 0])
-                b_val = np.mean(cropped[:, i][:, 1])
-                g_val = np.mean(cropped[:, i][:, 2])
+                b_val = np.mean(cropped[:, i][:, 0])  # index 0 = Blue
+                g_val = np.mean(cropped[:, i][:, 1])  # index 1 = Green
+                r_val = np.mean(cropped[:, i][:, 2])  # index 2 = Red
                 i_val = (r_val + b_val + g_val) / 3
 
                 r_dist.append(r_val)
                 g_dist.append(g_val)
                 b_dist.append(b_val)
                 i_dist.append(i_val)
-            
-            plt.subplot(2, 1, 1)
-            plt.imshow(frame[int(r[1]):int(r[1]+r[3]), int(r[0]):int(r[0]+r[2])])
 
+            # Display cropped image on top
+            plt.subplot(2, 1, 1)
+            cropped_rgb = cv2.cvtColor(cropped, cv2.COLOR_BGR2RGB)  # Convert BGR to RGB for display
+            plt.imshow(cropped_rgb)
+            plt.title('Selected ROI')
+            plt.axis('off')
+            
             plt.subplot(2, 1, 2)
-            plt.plot(r_dist, color='r', label='red')
+            plt.plot(r_dist, color='r', label='red')      
             plt.plot(g_dist, color='g', label='green')
             plt.plot(b_dist, color='b', label='blue')
             plt.plot(i_dist, color='k', label='mean')
+            plt.xlabel('Pixel Position')
+            plt.ylabel('Intensity')
+            plt.title('Spectrum Analysis')
             plt.legend(loc="upper left")
             plt.show()
 
